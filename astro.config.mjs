d258b36eeb2617ff7cc3b90e80c2b03004c5025b
CessarGarcia <cessar_garcia13@hotmail.com>
2024-01-20 20:40:23 -0600

    Add desployment with Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
+// import vercel
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
+  // Add Desploy with Vercek
   adapter: vercel()
 });
